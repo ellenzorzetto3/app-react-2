85667f06b7bd073f3229058e95caf0a51a8dec15
Ellen Zorzetto <ellen.zorzetto.it@gmail.com>
2024-07-15 18:50:00 -0300

    added api and api key

---
+++ src/Weather.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  const [temperature, setTemperature] = useState(null);
+
+  function useEffect() {
+    function handleApiResponse(response) {
+      setTemperature(response.data.main.temp);
+    }
+  }
+
+  const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  const units = "metric";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(handleApiResponse);
+
+  return (
+    <div>
+      {temperature ? (
+        <p>
+          The temperature in {props.city} is {temperature}°C
+        </p>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
